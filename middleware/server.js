6f058e121827740c3b1e7bd8f899b02cd89c0482
SudeepTrainer <info@icenbake.com>
2023-11-21 19:43:36 +0530

    21-11-2023

--- middleware/server.js
+++ middleware/server.js
@@ -2,15 +2,34 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//global middleware
+function logMiddleware(req,res,next){
+    console.log("log middleware called");
+    console.log("before");
+    if(req.query.username === "admin"){
+        next();
+    }else{
+        res.send("<h1>Only admins can access this page</h1>")
+    }
+    console.log("after");
+}
+
+app.use(logMiddleware);
+
 //default page
-app.get("/",defaultResponse);
 const defaultResponse = function (requestObject,responseObject,nextMiddleware){
     console.log("default page route action called");
     responseObject.send("<h1>Default page</h1>")
 }
+
+app.get("/",defaultResponse);
 //login page
-app.get("/login",loginResponse);
 const loginResponse = function(requestObject,responseObject,nextMiddleware){
     console.log("login page route action called");
     responseObject.send("<h1>Login page</h1>")
 }
+app.get("/login",loginResponse);
+
+app.listen(port,()=>{
+    console.log(`listening on the port ${port}`);
+})
