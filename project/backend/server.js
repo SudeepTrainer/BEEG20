d4296aed5f95f946e59395d1da68102fd7404e21
SudeepTrainer <info@icenbake.com>
2023-12-15 20:09:38 +0530

    15-12-2023

--- project/backend/server.js
+++ project/backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
-const session = require('express-session');
+// const session = require('express-session');
 const mongoose = require('mongoose');
-const mongodbStore = require('connect-mongodb-session')(session);
+// const mongodbStore = require('connect-mongodb-session')(session);
+const bcrypt  = require('bcryptjs');
+const cookieparser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 
 const databaseUri = "mongodb://127.0.0.1:27017/mydb";
 
@@ -19,30 +22,43 @@ db()
 
 const PORT = 3000;
 const application = express();
-const store = new mongodbStore({
-    uri:databaseUri,
-    collection:"sessions"
-});
+// const store = new mongodbStore({
+//     uri:databaseUri,
+//     collection:"sessions"
+// });
+// //middleware
+// application.use(session(
+//     {
+//         secret:"thisisasecrettosigncookie",
+//         resave:false,
+//         saveUninitialized:false,
+//         cookie:{
+//             secure:false,
+//             maxAge:60000
+//         },
+//         store:store
+//     }
+// ));
 //middleware
-application.use(session(
-    {
-        secret:"thisisasecrettosigncookie",
-        resave:false,
-        saveUninitialized:false,
-        cookie:{
-            secure:false,
-            maxAge:60000
-        },
-        store:store
-    }
-));
+
+application.use(cookieParser());
+application.use(express.urlencoded({extended:true}));
+
+application.set('view engine','ejs');
+
 //routing
 application.get("/",(req,res)=>{
-    req.session.isAuth = true;
-    console.log(req.session);
-    console.log(req.session.id);
-    res.send("<h1>Home page</h1>")
+    // req.session.isAuth = true;
+    // console.log(req.session);
+    // console.log(req.session.id);
+    res.render("home")
 });
+application.get("/login",(req,res)=>{
+    res.render('login');
+})
+application.get("/register",(req,res)=>{
+    res.render('register');
+})
 //start server
 application.listen(PORT,()=>{
     console.log(`Listening on ${PORT}`);
